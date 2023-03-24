65bd68490de17d87a3271bccb0c286ac2cc9343a
Sanjay <sanjayrjs16@gmail.com>
2023-03-25 05:17:17 +0530

    next.config.js updated to fix deployment issue

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
+}
+// https://github.com/ivansevillaa/use-next-blurhash/issues/4
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
